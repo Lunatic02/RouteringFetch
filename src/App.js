c70b5b4f1a125b70e35563a5ab3cf1711008c834
Lunatic02 <lucas_hcosta@live.com>
2023-03-01 21:03:26 -0300

    reestruturação do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Sobre from './Sobre';
-import Header from './Header';
-import Contato from './Contato';
-import Login from './Login';
-import Produto from './Produto';
 
-function App({ id }) {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="sobre" element={<Sobre />} />
-        <Route path="login" element={<Login />} />
-        <Route path="produto/:id" element={<Produto />}></Route>
-        <Route path="contato" element={<Contato />} />
-      </Routes>
-    </BrowserRouter>
-  );
+function App() {
+  return <div>oi</div>;
 }
 
 export default App;
